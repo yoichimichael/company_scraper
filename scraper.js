5e4103f9eb33eb5f3842acb5947d23c1650664bd
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-01 00:23:25 -0700

    load puppeteer to get full code

--- scraper.js
+++ scraper.js
@@ -1,18 +1,25 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 const url = 'https://blog.wealthfront.com/career-launching-companies-list/#companies-list'
 
-axios.get(url)
-  .then(res => {
-    console.log(res)
-    const html = res.data;
-    // console.log(html);
+puppeteer
+  .launch()
+  .then(browser => {
+    return browser.newPage();
+  })
+  .then(page => {
+    return page.goto(url).then(() => {
+      return page.content();
+    });
+  })
+  .then(html => {
     const $ = cheerio.load(html);
     const companies = [];
-    // $('.company_name').each((i, element) => {
-    //   console.log(element);
-    // });
-    // console.log(companies);
+    $('.company_name').each((i, element) => {
+      companies.push($(element).text());
+    });
+    console.log(companies);
   })
-  .catch(console.log)
+  .catch(console.log);
