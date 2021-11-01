c93fc9fceedc3c3132be7e35cc9a4ce57c03584d
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-31 23:53:47 -0700

    debug company name queries

--- scraper.js
+++ scraper.js
@@ -4,9 +4,15 @@ const cheerio = require('cheerio');
 const url = 'https://blog.wealthfront.com/career-launching-companies-list/#companies-list'
 
 axios.get(url)
-  .then(html => {
+  .then(res => {
+    console.log(res)
+    const html = res.data;
+    // console.log(html);
     const $ = cheerio.load(html);
-    const listContainer = $('.the-list-container');
-    console.log(listContainer.children());
+    const companies = [];
+    // $('.company_name').each((i, element) => {
+    //   console.log(element);
+    // });
+    // console.log(companies);
   })
   .catch(console.log)
