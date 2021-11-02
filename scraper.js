99ee37b39248dc3000337dd72b2beb686c47888e
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-02 00:00:05 -0700

    first db entry again

--- scraper.js
+++ scraper.js
@@ -24,10 +24,13 @@ puppeteer
     $('.pure-u-1.pure-u-md-18-24').each((i, element) => {
       const companyData = {
         name: element.children[1].children[0].data,
-      url: element.children[2].children[0].attribs.href,
+        url: element.children[2].children[0].attribs.href,
         location: element.nextSibling.children[0].children[0].data,
         description: element.nextSibling.children[1].children[0].data,
       }
+      const sqlQuery = 'INSERT INTO companies(id, name, url, location, description) VALUES(DEFAULT, $1, $2, $3, $4)';
+      const values = [companyData.name, this.price, this.description, this.imageUrl];
+      return db_pool.query(sqlQuery, values);
       companies.push(companyData);
     });
     console.log(companies);
