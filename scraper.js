cf182c16d5b716f95ab78ddb26f08ca0fb0ff04a
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-11-17 15:37:33 -0800

    write data to file

--- scraper.js
+++ scraper.js
@@ -1,9 +1,19 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 require('dotenv').config();
 
 const db_pool = require('./database');
+const csvWriter = createCsvWriter({
+  path: './companies.csv',
+  header: [
+      { id: 'name', title: 'COMPANY' },
+      { id: 'url', title: 'WEBSITE' },
+      { id: 'location', title: 'LOCATION' },
+      { id: 'description', title: 'ABOUT' },
+  ]
+});
 
 const url = 'https://blog.wealthfront.com/career-launching-companies-list/#companies-list'
 
@@ -30,9 +40,11 @@ puppeteer
       }
       const sqlQuery = 'INSERT INTO companies(id, name, url, location, description) VALUES(DEFAULT, $1, $2, $3, $4)';
       const values = [companyData.name, this.price, this.description, this.imageUrl];
-      return db_pool.query(sqlQuery, values);
+      // return db_pool.query(sqlQuery, values);
       companies.push(companyData);
     });
-    console.log(companies);
+    return companies;
   })
+  .then(companies => csvWriter.writeRecords(companies))
+  .then(result => console.log("Finished Writing File"))
   .catch(console.log);
